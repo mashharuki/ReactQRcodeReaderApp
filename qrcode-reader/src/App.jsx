f43d0862a3c2449a02036ff0975181a0ad6c4587
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-03-08 23:34:28 +0900

    修正

--- qrcode-reader/src/App.jsx
+++ qrcode-reader/src/App.jsx
@@ -12,6 +12,9 @@ const  App = () => {
   const [isOpenQRCamera, setIsOpenQRCamera] = useState(false);
   const [qrResult, setQrResult] = useState({});
 
+  /**
+   * clickOpenQrReader function
+   */
   const clickOpenQrReader = () => {
     setIsOpenQRCamera(true);
   };
@@ -19,8 +22,11 @@ const  App = () => {
   return (
     <Container maxWidth="md" style={{ paddingTop: "1em", paddingBottom: "10em" }}>
       <Button onClick={clickOpenQrReader}>test</Button>
+      <p>result: {qrResult}</p>
+      {/* QR code Reader */}
       {isOpenQRCamera && <QrCodeReader onRead={e => {
         setIsOpenQRCamera(false);
+        setQrResult(e);
         console.log(e);
       }} setOpen={setIsOpenQRCamera} />}
     </Container >
